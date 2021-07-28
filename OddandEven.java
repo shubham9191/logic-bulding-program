0d184f14a11ad14182aec84e62c8849fc6402041
SHUBHAM SAHU <70929748+shubham9191@users.noreply.github.com>
2021-07-28 15:21:35 +0530

    Create OddandEven.java

---
+++ OddandEven.java
@@ -0,0 +1,22 @@
+package JavaProgram;
+
+import java.util.Scanner;
+
+public class OddandEven {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.printf("enter the number:");
+		int x=s.nextInt();
+		
+		if(x%2==0) {
+			System.out.println("Even");
+		}
+		else {
+		System.out.println("Odd");
+
+	}
+	}
+	
+
+}
