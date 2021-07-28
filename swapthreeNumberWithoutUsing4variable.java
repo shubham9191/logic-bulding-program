05f448f518a63e8bf9d130bc350a8a6d2e301e81
SHUBHAM SAHU <70929748+shubham9191@users.noreply.github.com>
2021-07-28 09:15:04 +0530

    Create swapthreeNumberWithoutUsing4variable.java

---
+++ swapthreeNumberWithoutUsing4variable.java
@@ -0,0 +1,22 @@
+package JavaProgram;
+
+import java.util.Scanner;
+
+public class swapTwoNumber {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		int y=s.nextInt();
+		int z=s.nextInt();
+		
+		x=x+y+z;
+		y=x-(y+z);
+		z=x-(y+z);
+		x=x-(y+z);
+		
+		System.out.println(x+"  "+y+" "+z);
+
+	}
+
+}
